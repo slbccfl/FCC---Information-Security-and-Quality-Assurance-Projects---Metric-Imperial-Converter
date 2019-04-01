f8786d67b0dd7a89196e2d7db959fb395b448243
Glitch (turquoise-shear) <none>
2019-04-01 17:29:07 +0000

    Continued work on conversions

--- controllers/convertHandler.js
+++ controllers/convertHandler.js
@@ -10,7 +10,7 @@ function ConvertHandler() {
   
   this.getNum = function(input) {
     var result;
-    result = input.match('/\d+(\.\d+)?/')
+    result = input.match(/\d+(\.\d+)?/)[0]
     console.log(result);
     return result;
   };
@@ -30,14 +30,16 @@ function ConvertHandler() {
   };
   
   this.getReturnUnit = function(initUnit) {
+    var unitPairs = {'gal':'L', 'L':'gal', 'lbs':'kg', 'kg':'lbs', 'mi':'km', 'km': 'mi'}
     var result;
-    
+    result = unitPairs[initUnit];
     return result;
   };
 
   this.spellOutUnit = function(unit) {
+    var spelledOutUnits = {'gal':'gallons', 'L':'liters', 'lbs':'pounds', 'kg':'kilograms', 'mi':'miles', 'km': 'kilometers'}
     var result;
-    
+    result = spelledOutUnits[unit];
     return result;
   };
   
@@ -45,8 +47,9 @@ function ConvertHandler() {
     const galToL = 3.78541;
     const lbsToKg = 0.453592;
     const miToKm = 1.60934;
+    var convertFactor = {'gal':3.78541, 'L':3.78541/1, 'lbs':0.453592, 'kg':0.453592/1, 'mi':1.60934, 'km': 1.60934/1}
     var result;
-    
+    result = initNum * convertFactor[initUnit];
     return result;
   };
   
